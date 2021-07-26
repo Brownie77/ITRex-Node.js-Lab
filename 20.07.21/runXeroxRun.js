51b1b31c34a7998efeabe9bacd838e4e8184b140
Kyrylo Podolynnyi <Kirill.podolinniy@gmail.com>
2021-07-26 12:19:19 +0300

    Update: delete some entities from printer task

--- 20.07.21/runXeroxRun.js
+++ 20.07.21/runXeroxRun.js
@@ -3,17 +3,14 @@ function copy(n, x, y) {
     let maxTime = (n-1)* Math.min(x,y);
     while(maxTime- minTime>1){
         let middle = (maxTime+minTime)/2;
-        console.log(Math.floor(middle/y)+Math.floor(middle/x))
         if(Math.floor(middle/y)+Math.floor(middle/x)>=n-1){
             maxTime = Math.floor(middle);
         } else {
             minTime = Math.floor(middle);
         }
-        console.log(maxTime)
-
     }
     return (maxTime + Math.min(x,y))
 }
 
 
-console.log(copy(5, 1, 2))
+console.log(copy(5, 1, 2))  
